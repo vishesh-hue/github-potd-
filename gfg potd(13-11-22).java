aff8a76bf814ce225817af10c66ecfcfed80ce69
vishesh tiwari <tvishesh593@gmail.com>
2022-11-13 22:31:53 +0530

    Create gfg potd(13-11-22).java

---
+++ gfg potd(13-11-22).java
@@ -0,0 +1,11 @@
+class Solution{
+    public int totalWays(int n, int[] arr) {
+        Arrays.sort(arr);
+        int mod=1000000007;
+        long ans=1;
+        for(int i=0;i<n;i++){
+            ans=(ans*(arr[i]-i))%mod;
+        }
+        return (int)ans%mod;
+    }
+}
