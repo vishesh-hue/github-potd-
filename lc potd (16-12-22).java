c7b90513e69bd69bab5b95ac83b30b8ae125f38b
vishesh tiwari <tvishesh593@gmail.com>
2022-12-16 21:45:43 +0530

    Create lc potd (16-12-22).java

---
+++ lc potd (16-12-22).java
@@ -0,0 +1,31 @@
+class MyQueue {
+    Stack<Integer> s1;
+    Stack<Integer> s2;
+
+    public MyQueue() {
+        s1 = new Stack<Integer>();
+        s2 = new Stack<Integer>();
+    }
+
+    public void push(int x) {
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+        s1.push(x);
+        while (!s2.isEmpty()) {
+            s1.push(s2.pop());
+        }
+    }
+
+    public int pop() {
+        return s1.pop();
+    }
+
+    public int peek() {
+        return s1.peek();
+    }
+
+    public boolean empty() {
+        return s1.isEmpty();
+    }
+}
